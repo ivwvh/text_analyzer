9935dbb20ca269fadfbfc177f0641289803c6d1e
ivwvh <68565784+ivwvh@users.noreply.github.com>
2023-06-07 22:11:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from encoding_checker import check_encoding
 """
 
 Скачать текст
@@ -10,8 +10,18 @@
 
 class TextAnalyzer:
     def __init__(self) -> None:
-        self.text = open("./test.txt", '+r', encoding="utf-8").read()
+        self.text_encoding = input("Введите название кодировки: ")
+        if check_encoding(self.text_encoding):
+            self.text = None
+            self.get_text()
+            self.print_text()
+        else:
+            print("Кодировки не существует")
+    
+    def get_text(self) -> None:
+        self.text = open("./test.txt", '+r', encoding=self.text_encoding).read()
+    
+    def print_text(self):
         print(self.text)
-
 
 TextAnalyzer()
