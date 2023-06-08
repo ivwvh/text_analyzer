ff1aaf3e70eb8b053e3c1d475b2a640011207abb
ivwvh <68565784+ivwvh@users.noreply.github.com>
2023-06-08 19:57:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 """
 
 Скачать текст
@@ -8,23 +10,24 @@
 
 
 class TextAnalyzer:
-    def __init__(self) -> None:
+    def __init__(self, file_path=None, text_encoding="utf-8") -> None:
         self.text_encoding = input("Введите название кодировки: ")
+        self.file_path = Path(file_path)
         if not self.text_encoding:
-            self.text_encoding = "utf_8"
-            self.text = None
-            self.get_text()
-            self.print_text()
-        else:
-            self.text = None
-            self.get_text()
-            self.print_text()
+            self.text_encoding = text_encoding
+        if not file_path:
+            self.file_path = Path(input("Введите путь до файла: "))
+
+        self.get_text()
+        self.print_text()
 
     def get_text(self) -> None:
-        self.text = open("./test.txt", '+r', encoding=self.text_encoding).read()
-    
-    def print_text(self):
+        self.text = open(self.file_path,
+                         '+r',
+                         encoding=self.text_encoding).read()
+
+    def print_text(self) -> None:
         print(self.text)
 
 
-TextAnalyzer()
+TextAnalyzer("")
