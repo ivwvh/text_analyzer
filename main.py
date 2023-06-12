05ab16a6023a51ba4949a7e28746b83fe66a98f2
ivwvh <68565784+ivwvh@users.noreply.github.com>
2023-06-12 18:39:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from sys import exit
+
 """
 
 Скачать текст
@@ -12,22 +12,30 @@ from sys import exit
 class TextAnalyzer:
     def __init__(self, file_path=None, text_encoding="utf-8") -> None:
         if not file_path:
-            print("Не указан путь к файлу")
-            exit()
+            raise Exception("Не указан путь к файлу")
         self.file_path = Path(file_path)
         self.text_encoding = text_encoding
 
         self.open_file()
         self.get_text()
+        self.make_lower()
         self.print_text()
 
     def open_file(self) -> None:
-        self.text = open(self.file_path,
-                         '+r',
-                         encoding=self.text_encoding)
+        try:
+            self.text = open(self.file_path,
+                             '+r',
+                             encoding=self.text_encoding)
+        except FileNotFoundError:
+            raise Exception("Файл не найден")
 
-    def get_text(self):
+    def get_text(self) -> str:
         self.text = self.text.read()
+        if not len(self.text):
+            raise Exception("Файл пуст")
+
+    def make_lower(self) -> str:
+        self.text = self.text.lower()
 
     def print_text(self) -> None:
         print(self.text)
