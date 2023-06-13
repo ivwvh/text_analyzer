5961c6b3e20801368b737705f800ae0a1a39b692
ivwvh <68565784+ivwvh@users.noreply.github.com>
2023-06-13 11:26:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import NoReturn
+from string import punctuation
 
 """
 
@@ -19,26 +21,43 @@ class TextAnalyzer:
         self.open_file()
         self.get_text()
         self.make_lower()
-        self.print_text()
+        self.get_words()
+        self.clean_text()
 
-    def open_file(self) -> None:
+    def open_file(self) -> None | NoReturn:
         try:
-            self.text = open(self.file_path,
-                             '+r',
-                             encoding=self.text_encoding)
+            file = open(self.file_path,
+                      '+r',
+                      encoding=self.text_encoding)
+            self.file = file
         except FileNotFoundError:
             raise Exception("Файл не найден")
 
-    def get_text(self) -> str:
-        self.text = self.text.read()
-        if not len(self.text):
-            raise Exception("Файл пуст")
+    def get_text(self) -> None:
+        self.text = self.file.read()
 
-    def make_lower(self) -> str:
+    def check_empty_file(self) -> RuntimeError | None:
+        if not self.text:
+            raise RuntimeError("Файл пуст")
+
+    def make_lower(self) -> None:
         self.text = self.text.lower()
 
     def print_text(self) -> None:
         print(self.text)
+
+    def get_words(self) -> None:
+        self.words = self.text.split()
+
+    def clean_text(self) -> None:
+        for i in range(len(self.words)):
+            for symbol in self.words[i]:
+                if symbol in punctuation:
+                    punct = symbol
+                    self.words[i] = self.words[i].replace(punct, "")
+        for i in range(self.words.count("")):
+            self.words.remove("")
+        print(self.words)
 
 
 TextAnalyzer(file_path="./test.txt")
